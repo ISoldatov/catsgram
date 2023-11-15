cff348b3182e295427bc9c1202a6dc5e8c37a2be
Ivan Soldatov <131395398+ISoldatov@users.noreply.github.com>
2023-11-15 17:24:59 +0300

    реализован UserController

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,26 +1,49 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.util.exception.InvalidEmailException;
+import ru.yandex.practicum.catsgram.util.exception.UserAlreadyExistException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    private List<User> users = new ArrayList<>();
+    private Set<User> users = new HashSet<>();
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAll() {
-        return users;
+        return new ArrayList<>(users);
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public User create(@RequestBody User user) {
+        String email = user.getEmail();
+        if (email == null || email.isBlank() || !email.contains("@")) {
+            throw new InvalidEmailException(user);
+        }
+
+        if (users.contains(user)) {
+            throw new UserAlreadyExistException(user);
+        } else {
+            users.add(user);
+        }
+
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        String email = user.getEmail();
+        if (email == null || email.isBlank() || !email.contains("@")) {
+            throw new InvalidEmailException(user);
+        }
+        users.remove(user);
         users.add(user);
         return user;
     }
